6b1ab646b7a856647c53ff56aa7193f82e4b38ea
ayushmaannsingh <sa776857@gmail.com>
2025-08-30 23:10:17 +0530

    first added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 
-
+import Ayush from "./Ayush";
 
 function App() {
   return (
+    <>
+    <Ayush/>
      <h1>Hello Ayush</h1>
+     <h3>kaise ho</h3>
+     <p>kya hua hain</p>
+     
+    </>
   );
 }
 
