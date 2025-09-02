05efbc251585ddb2740c716a3ad77641475314fb
ayushmaannsingh <sa776857@gmail.com>
2025-09-02 21:35:09 +0530

    Fixed basicreact folder (converted from submodule)

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,18 @@
+
+
+
+import Ayush from "./Ayush";
+
+function App() {
+  return (
+    <>
+    <Ayush/>
+     <h1>Hello Ayush</h1>
+     <h3>kaise ho</h3>
+     <p>kya hua hain</p>
+     
+    </>
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/Ayush.js
@@ -0,0 +1,12 @@
+function Ayush(){
+    return(
+        <>
+        <h2>Learning react</h2>
+        <h3>kyaa haal hain</h3>
+        <p>kya kar rhe ho</p>
+        </>
+        
+    )
+}
+
+export default Ayush;
