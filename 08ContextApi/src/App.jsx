021ea3188b1c2c484b3e3c823c7a5019010ee925
ayushmaannsingh <sa776857@gmail.com>
2025-09-07 02:05:23 +0530

    Context Api

--- 08ContextApi/src/App.jsx
+++ 08ContextApi/src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Login from './components/Login'
+import Profile from './components/Profile'
+import UserContextProvider from './context/UserContextProvider'
 
 function App() {
   
+
   return (
-    <>
-      <h1 >React Context API</h1>
-    </>
+    <UserContextProvider>
+      <h1>Hey this side Ayush Singh learning React Js</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
   )
 }
 
