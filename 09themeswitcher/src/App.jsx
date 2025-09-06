698ae95cb9e286974ae3d99476992c189fbeb34e
ayushmaannsingh <sa776857@gmail.com>
2025-09-07 03:03:02 +0530

    theme switcher

---
+++ 09themeswitcher/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div className="text-5xl font-bold text-white bg-orange-500">Ayush Singh</div>
+    </>
+  )
+}
+
+export default App
